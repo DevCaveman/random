ee13cc45f43ade5cc34f49f251423a51cd6d5750
DevThiago <devthiago.py@gmail.com>
2018-08-13 19:09:54 -0300

    Making big changes

--- Teste/divdas/class/arquivo.py
+++ Teste/divdas/class/arquivo.py
@@ -6,18 +6,18 @@ import json
 class Arquivo:
 
     def __init__(self, nome_arquivo : str):
-        self.__nome_arquivo = nome_arquivo + '.json'
+        self._nome_arquivo = nome_arquivo + '.json'
         
     def ler(self):
         try:
-            with open(self.__nome_arquivo, 'r') as arquivo_contas:
+            with open(self._nome_arquivo, 'r') as arquivo_contas:
                 return json.load(arquivo_contas)
         except Exception as err:
             print('Erro -> {}'.format(err))
 
     def escrever(self, data_to_write : dict):
         try:
-            with open(self.__nome_arquivo, 'w') as arquivo_contas:
+            with open(self._nome_arquivo, 'w') as arquivo_contas:
                 json.dump(data_to_write, arquivo_contas)
         except Exception as err:
             print('Erro -> {}'.format(err))
---
+++ Teste/divdas/class/conta.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import json
+from arquivo import Arquivo
+
+class Conta(object, Arquivo):
+    pass
