5caf9b299d1075cf3c1b24354df566c8de011847
DevThiago <devthiago.py@gmail.com>
2018-08-13 21:50:57 -0300

    Big changes

---
+++ Teste/divdas/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from Classes.arquivo import Arquivo
+
+conta = Arquivo.make_contas()
+dicionario = { 'penis': 'pequeno' }
+conta.adicionar(dicionario)
+conta.ler()
+dicionario = { 'teta': 'seca' }
+conta.adicionar(dicionario)
+conta.ler()
