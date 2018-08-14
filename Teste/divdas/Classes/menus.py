83ed22e7aebd86214e370d90d77bf8eac8ddee14
DevThiago <devthiago.py@gmail.com>
2018-08-13 21:51:27 -0300

    my_classes -> Classes

---
+++ Teste/divdas/Classes/menus.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Menus(Object):
+    
+    def __init__(self):
+        pass
+
+    @classmethod
+    def make_menu_principal(cls):
+        pass
+
+    @classmethod
+    def make_menu_inserir_conta(cls):
+        pass
+
+    @classmethod
+    def make_menu_ler_contas(cls):
+        pass
